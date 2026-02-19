a92858dcc7f02cac3c46754fdb5d2ab402a13ab6
ROHITH <rohithperumal63@gmail.com>
2026-02-19 14:06:52 +0530

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,35 @@
+class ArrayQueue:
+
+    def __init__(self):
+        self.data=[]
+        self.size=0
+        self.front=0
+    def __len__(self):
+        return self.size
+    def is_empty(self):
+        return self.size==0
+    def enqueue(self,e):
+        self.data.append(e)
+        self.size += 1
+    def dequeue(self):
+        if self.is_empty():
+            raise empty("queue is empty")
+        value=self.data[self.front]
+        self.data[self.front] = None
+        self.front +=1
+        self.size -=1
+        return value
+    def first(self):
+        if self.is_empty():
+            raise empty("queue is empty")
+        return self.data[self.front]
+
+
+q = ArrayQueue()
+q.enqueue(64)
+q.enqueue(77)
+q.enqueue(87)
+print(q.dequeue())
+print(q.is_empty())
+print(q.first())
+
